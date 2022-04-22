14779092051dda093c903b7cb32838bf8db5b99c
ASHLEYANDRES <aandres7@hawaii.edu>
2022-04-22 12:19:36 -1000

    server?

--- Client Side Pages/server.js
+++ Client Side Pages/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./static'));
+app.use(express.Pages('./Pages'));
 app.listen(8080, () => console.log(`listening on port 8080`));
--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//create a server object:
-http.createServer(function (req, res) {
-    console.log(req.headers); //output the request headers to the console
-    res.writeHead(200, { 'Content-Type': 'text/html' }); // set MIME type to HTML 
-    res.write(`<h1>The server date is: ${Date.now()}</h1>`); //send a response to the client
-    res.write('<h1>The client date is: <script>document.write( Date.now() );</script></h1>'); // send another response
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-
-console.log('Hello world HTTP server listening on localhost port 8080');
+app.use(express.static('./static'));
+app.listen(8080, () => console.log(`listening on port 8080`));
