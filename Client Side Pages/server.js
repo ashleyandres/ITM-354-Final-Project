e3cbc04f305d6d719be067940d31b4988195b2dc
ASHLEYANDRES <aandres7@hawaii.edu>
2022-04-22 13:16:32 -1000

    Update server.js

--- Client Side Pages/server.js
+++ Client Side Pages/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use(express.Pages('./Pages'));
+app.use(express.static('./Pages/homepage.html'));
 app.listen(8080, () => console.log(`listening on port 8080`));
